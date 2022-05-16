259c3c7bd04f1110e664b72ab6652eb8b249b195
Thierbach <81238576+Thierbach@users.noreply.github.com>
2022-05-16 08:37:18 +0200

    Add files via upload

--- cf-context1.js
+++ cf-context1.js
@@ -2,10 +2,22 @@ window.onload = function() {
   var conversationalForm = window.cf.ConversationalForm.startTheConversation({
     formEl: document.getElementById("form"),
     context: document.getElementById("cf-context"),    
+    showProgressBar: true, 
     dictionaryData: {
         "input-placeholder": "Gib bitte hier Deine Antwort ein ...",
-        "user-reponse-missing": "Fehlende Eingabe ..."
+        "user-reponse-missing": "Fehlende Eingabe ...",
+        "user-reponse": "Keine Auswahl getroffen ...",
+        "input-placeholder": "Geben Sie hier Ihre Antwort ein ...",
+        "input-placeholder-error": "Ihre Eingabe ist nicht korrekt ...",
+        "input-no-filter": "Keine Ergebnisse für {input-value}",
+        "general": "Allgemeiner Typ1|Allgemeiner Typ2",
+        "input-placeholder-required": "Eingabe erforderlich ...",
+        "user-response-and": "Beantworte bitte die Frage ..."
     },
+    dictionaryRobot: {
+    	"checkbox": "Wählen Sie so viele, wie Sie möchten."
+    },	
+    
     submitCallback: function() {
       var formData = conversationalForm.getFormData();
                     var formDataSerialized = conversationalForm.getFormData(true);
